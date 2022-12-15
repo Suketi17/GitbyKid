e6d6aa525b6842fa1f7689127c489b54faf91e0b
Suketi17 <nguyenduyhau.dct@gmail.com>
2022-12-15 09:58:40 +0700

    lan12

--- test.caerue/Object Repository/Folder_Gerenal/訪問先/DoiLich/Day2.rs
+++ test.caerue/Object Repository/Folder_Gerenal/訪問先/DoiLich/Day2.rs
@@ -6,11 +6,11 @@
    <elementGuidId>4fffa384-31e9-4e55-9474-f02151d36c23</elementGuidId>
    <selectorCollection>
       <entry>
-         <key>XPATH</key>
-         <value>//body/main[@id='attendance_detail']/section[2]/section[1]/section[4]/section[1]/div[1]/section[2]/table[1]/tr[3]/td[1]</value>
+         <key>BASIC</key>
       </entry>
       <entry>
-         <key>BASIC</key>
+         <key>XPATH</key>
+         <value>/html[1]/body[1]/main[1]/section[2]/section[1]/section[4]/section[1]/div[1]/section[2]/table[1]/tr[3]/td[1]</value>
       </entry>
    </selectorCollection>
    <selectorMethod>XPATH</selectorMethod>
